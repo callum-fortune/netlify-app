8ec0ee3e95d292ac7d324cf13534450d0b07f5b6
Callum Fortune <callumfortune03@outlook.com>
2022-03-24 19:24:11 +0000

    1

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -20,10 +20,10 @@ class Navigation extends Component {
             <span>
                 <h1 className="nav-header">&lt;callumfortune.com&gt;</h1>
                 <ul className={`nav-links-container ${this.state.toggleOpen ? 'nav-mobile-open' : ''}`}>
-                    <li className="nav-link"><a href="#">Home</a></li>
-                    <li className="nav-link"><a href="#">About me</a></li>
-                    <li className="nav-link"><a href="#">Projects</a></li>
-                    <li className="nav-link"><a href="#"><button>Log in</button></a></li>
+                    <li className="nav-link"><a href="/">Home</a></li>
+                    <li className="nav-link"><a href="/">About me</a></li>
+                    <li className="nav-link"><a href="/">Projects</a></li>
+                    <li className="nav-link"><a href="/"><button>Log in</button></a></li>
                 </ul>
                 <button className="nav-toggle" onClick={this.toggleNav}>{this.state.toggleOpen ? <i className="fa fa-times"></i> : <i className="fa fa-bars"></i>}</button>
             </span>
